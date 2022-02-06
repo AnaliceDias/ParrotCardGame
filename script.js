a6b62c6907577c30f0aa586c6f2e3f4386983feb
Analice Dias <anadias.alice@gmail.com>
2022-02-06 01:18:55 -0300

    Correção automágica

--- script.js
+++ script.js
@@ -1,57 +1,57 @@
 const cartasDoJogo = [
-    `<div class="carta" onclick="virarCarta(this)">
-    <div class="frente face">
+    `<div class="carta" onclick="mostrarCarta(this, 1)" data-identifier="card" >
+    <div class="frente face" data-identifier="back-face">
         <img src="imagens/front.png">
     </div>
-    <div class="verso face">
+    <div class="verso face" data-identifier="front-face">
         <img src="imagens/metalparrot.gif">
     </div>
     </div> `,
-    `<div class="carta" onclick="virarCarta(this)">
-    <div class="frente face">
+    `<div class="carta" onclick="mostrarCarta(this, 2)" data-identifier="card" >
+    <div class="frente face" data-identifier="back-face">
         <img src="imagens/front.png">
     </div>
-    <div class="verso face">
+    <div class="verso face" data-identifier="front-face">
         <img src="imagens/bobrossparrot.gif">
     </div>
     </div> `,
-    `<div class="carta" onclick="virarCarta(this)">
-    <div class="frente face">
+    `<div class="carta" onclick="mostrarCarta(this, 3)" data-identifier="card" >
+    <div class="frente face" data-identifier="back-face">
         <img src="imagens/front.png">
     </div>
-    <div class="verso face">
+    <div class="verso face" data-identifier="front-face">
         <img src="imagens/explodyparrot.gif">
     </div>
     </div> `,
-    `<div class="carta" onclick="virarCarta(this)">
-    <div class="frente face">
+    `<div class="carta" onclick="mostrarCarta(this, 4)" data-identifier="card" >
+    <div class="frente face" data-identifier="back-face">
         <img src="imagens/front.png">
     </div>
-    <div class="verso face">
+    <div class="verso face" data-identifier="front-face">
         <img src="imagens/fiestaparrot.gif">
     </div>
     </div> `,
-    `<div class="carta" onclick="virarCarta(this)">
-    <div class="frente face">
+    `<div class="carta" onclick="mostrarCarta(this, 5)" data-identifier="card" >
+    <div class="frente face" data-identifier="back-face">
         <img src="imagens/front.png">
     </div>
-    <div class="verso face">
+    <div class="verso face" data-identifier="front-face">
         <img src="imagens/revertitparrot.gif">
     </div>
     </div> `,
-    `<div class="carta" onclick="virarCarta(this)">
-    <div class="frente face">
+    `<div class="carta" onclick="mostrarCarta(this, 6)" data-identifier="card" >
+    <div class="frente face" data-identifier="back-face">
         <img src="imagens/front.png">
     </div>
-    <div class="verso face">
+    <div class="verso face" data-identifier="front-face">
         <img src="imagens/tripletsparrot.gif">
     </div>
     </div> `,
-    `<div class="carta" onclick="virarCarta(this)">
-    <div class="frente face">
+    `<div class="carta" onclick="mostrarCarta(this, 7)" data-identifier="card" >
+    <div class="frente face" data-identifier="back-face">
         <img src="imagens/front.png">
     </div>
-    <div class="verso face">
+    <div class="verso face" data-identifier="front-face">
         <img src="imagens/unicornparrot.gif">
     </div>
     </div> `
@@ -60,16 +60,79 @@ const cartasDoJogo = [
 
 let cartasUsadas = [];
 let numeroDeCartas = 0;
+let permitirJogada = true;
+let contadorDeCliquePorJogada = 0;
+let cartasClicadas = [];
+let referenciasDasCartasClicadas = [];
 
 function comparador() { 
 	return Math.random() - 0.5; 
 }
 
-function virarCarta(carta){
-    carta.classList.toggle("virada");
+function validarPar(carta1,referenciaCarta1, carta2,referenciaCarta2){
+    
+    if(referenciaCarta1===referenciaCarta2){
+        carta1.classList.add("encontrouOPar");
+        carta2.classList.add("encontrouOPar");
+    }else{
+        carta1.classList.remove("virada");
+        carta2.classList.remove("virada");
+        //let comando1 = setInterval(comando2, 500);
+        //setInterval(carta1.classList.remove("virada"), 500);
+        //setInterval(carta2.classList.remove("virada"), 500);
+    
+        //clearInterval(comando2);
+    } 
 
+    cartasClicadas=[];
+    referenciasDasCartasClicadas=[];
 }
+function comando1(){
+    permitirJogada = true;
+    
+}
+function mostrarCarta(carta, referenciaDoPar){
+    let classesDaCarta = carta.classList;
 
+    for (let i = 0; i< classesDaCarta.length; i++){
+        if(classesDaCarta[i]==="encontrouOPar"){
+            return;
+        }
+    }
+
+    cartasClicadas.push(carta);
+    referenciasDasCartasClicadas.push(referenciaDoPar);
+    contadorDeCliquePorJogada++;
+    console.log("contador = "+contadorDeCliquePorJogada)
+    
+    if ((contadorDeCliquePorJogada<2) && (permitirJogada===true)){
+        carta.classList.toggle("virada");
+        
+    }else if((contadorDeCliquePorJogada===2) && (permitirJogada===true)){
+        carta.classList.toggle("virada");
+        permitirJogada = false;
+        contadorDeCliquePorJogada=0;
+        let comando = setInterval(comando1, 1000);
+        clearInterval(comando);
+        validarPar(cartasClicadas[0], referenciasDasCartasClicadas[0], cartasClicadas[1], referenciasDasCartasClicadas[1]);
+        //setTimeout(permitirJogada = true, 1000);
+        //setTimeout(contadorDeCliquePorJogada=0, 1000);
+        
+    }
+    else {
+        permitirJogada = false;
+        //let comando1 = setInterval(`permitirJogada = true`, 1000);
+        //let comando2 = setInterval(`contadorDeCliquePorJogada=0`, 1000);
+        let comando = setInterval(comando1, 1000);
+        clearInterval(comando);
+        //clearInterval(comando2);
+        validarPar(cartasClicadas[0], referenciasDasCartasClicadas[0], cartasClicadas[1], referenciasDasCartasClicadas[1]);
+        //setTimeout(permitirJogada = true, 1000);
+        //setTimeout(contadorDeCliquePorJogada=0, 1000);
+    }
+    
+}
+    
 function adicionarCartasATela(cartasUsadas){
     let main = document.querySelector("main");
     for(let i = 0; i< cartasUsadas.length; i++){
